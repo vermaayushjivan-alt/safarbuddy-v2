ceb71df418ab42109129b3ee615a57acf4b9bb1e
vermaayushjivan-alt <vermaayushjivan@gmail.com>
2026-08-02 15:04:40 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Hero from "@/components/home/Hero";
 import Offers from "@/components/home/Offers";
 import Destinations from "@/components/home/Destinations";
 import Trending from "@/components/home/Trending";
+import TrendingFlights from "@/components/home/TrendingFlights";
 import Packages from "@/components/home/Packages";
 import Testimonials from "@/components/home/Testimonials";
 import AppDownload from "@/components/home/AppDownload";
 import Footer from "@/components/home/Footer";
-
 export default function Home() {
   return (
     <main className="bg-cream">
@@ -16,6 +16,7 @@ export default function Home() {
       <Offers />
       <Destinations />
       <Trending />
+      <TrendingFlights />
       <Packages />
       <Testimonials />
       <AppDownload />
