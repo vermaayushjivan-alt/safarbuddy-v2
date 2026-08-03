8ff873b07000efd3254b1dbd2f4ea501bdaea643
vermaayushjivan-alt <vermaayushjivan@gmail.com>
2026-08-03 10:46:07 -0700

    Update index.ts

--- src/lib/repository/index.ts
+++ src/lib/repository/index.ts
@@ -1,4 +1,6 @@
 export { BaseRepository } from './BaseRepository';
+export { UserRepository } from './UserRepository';
+export { RepositoryFactory, createRepositories } from './repositories';
 export type {
   PaginationOptions,
   PaginationResult,
