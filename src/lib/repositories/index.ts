668e11169ef4a298d95f49cdd954d20f48ae2305
vermaayushjivan-alt <vermaayushjivan@gmail.com>
2026-08-03 11:41:47 -0700

    Create index.ts

---
+++ src/lib/repositories/index.ts
@@ -0,0 +1,9 @@
+export { BaseRepository } from './base.repository';
+export { UserRepository } from './user.repository';
+export {
+  RepositoryError,
+  NotFoundError,
+  ValidationError,
+  DatabaseError,
+  ConflictError,
+} from './errors';
