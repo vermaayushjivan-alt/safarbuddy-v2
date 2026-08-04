349e9483b78bd8ebe49a7fab0bfc4170e70ce64c
vermaayushjivan-alt <vermaayushjivan@gmail.com>
2026-08-04 11:14:27 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chnybctlzagtdrggjjhg.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
