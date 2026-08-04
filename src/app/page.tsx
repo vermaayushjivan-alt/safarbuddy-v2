f08b97bf32c392aa7cf60d49cccafc69c64c878c
vermaayushjivan-alt <vermaayushjivan@gmail.com>
2026-08-03 23:51:44 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-import Newsletter from "@/components/home/Newsletter";
-import Navbar from "@/components/home/Navbar";
-import Hero from "@/components/home/Hero";
-import Offers from "@/components/home/Offers";
-import Destinations from "@/components/home/Destinations";
-import Trending from "@/components/home/Trending";
-import TrendingFlights from "@/components/home/TrendingFlights";
-import Packages from "@/components/home/Packages";
-import Testimonials from "@/components/home/Testimonials";
-import AppDownload from "@/components/home/AppDownload";
-import Footer from "@/components/home/Footer";
 export default function Home() {
   return (
-    <main className="bg-cream">
-      <Navbar />
-      <Hero />
-      <Offers />
-      <Destinations />
-      <Trending />
-      <TrendingFlights />
-      <Packages />
-      <Testimonials />
-      <AppDownload />
-      <Newsletter />
-      <Footer />
+    <main>
+      <h1>SafarBuddy Test</h1>
     </main>
   );
 }
